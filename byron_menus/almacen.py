a0393eda14dd8feacba5a660334811f4ae612627
Byron Josué Par Rancho <3237971560511@ingenieria.usac.edu.gt>
2020-07-12 00:30:09 -0600

    culminando grafo 1

--- byron_menus/almacen.py
+++ byron_menus/almacen.py
@@ -64,9 +64,34 @@ def imprimir(id, fecha, hora):
 
 
 def generar_grafo(link1, link2):
+    from graphviz import Digraph
     url = link1+link2
     url = url[1 : -1]  # le quito las comillas dobles a la url
-    
+    f = Digraph(format='jpg', name='Almacen de Caracteres', directory="C:\\Users\\ByronJosué\\Desktop",
+                node_attr={'shape': 'plaintext'})
+    f.attr(rankdir='LR', size='9,5')
+    # Creación de nodos (tabla)
+
+    tabla_variables = '''<
+    <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4" ALIGN="CENTER">
+      <TR>
+        <TD COLSPAN="2" WIDTH="180" ALIGN="CENTER"><B>Tabla de Variables</B></TD>
+      </TR>
+      <TR>
+        <TD COLSPAN="1" WIDTH="150" ALIGN="LEFT"><B>ID</B></TD>
+        <TD WIDTH="150" COLSPAN="1" ALIGN="LEFT"><B>POSICIÓN</B></TD>
+      </TR>'''
+
+    for token in TOKENS:    # COMPLETO LA TABLA COLOCANDOLE CADA UNO DE LOS VALORES
+        tabla_variables += f'''\n<TR>
+        <TD COLSPAN="1" WIDTH="150" ALIGN="LEFT">{token.id}</TD>
+        <TD WIDTH="150" COLSPAN="1" ALIGN="LEFT">{token.posicion}</TD>
+      </TR>'''
+
+    tabla_variables += '\n</TABLE>>'  # culmino la tabla
+    f.node('struct3', tabla_variables)
+    # Creación del grafo
+    f.render()
 
 
 
@@ -101,7 +126,7 @@ def splitDatos(datos):
 
         elif inst_y_dat[0] == "Generar_grafo":
 
-            generar_grafo(inst_y_dat[1], inst_y_dat[2])
+            generar_grafo("inst_y_dat[1]", "inst_y_dat[2]")
 
 
     newrut = "C:\\Users\\ByronJosué\\Desktop\\almacenes.almacen_result"
